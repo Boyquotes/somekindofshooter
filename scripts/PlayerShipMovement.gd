0643f54929d7ba9a1e2e08e22dbadef6624d8722
ady <ady84.ro@gmail.com>
2023-05-23 18:38:32 +0300

    Player ship basic movement

---
+++ scripts/PlayerShipMovement.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+const SPEED = 300
+const FRICTION = 100
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	input_vector = input_vector.normalized()
+	
+	velocity = velocity.move_toward(input_vector * SPEED, FRICTION)
+	
+	move_and_slide(velocity)

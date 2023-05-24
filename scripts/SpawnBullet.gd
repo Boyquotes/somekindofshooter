2ca06459cd912392faf5ed678baa27f00e81307d
ady <ady84.ro@gmail.com>
2023-05-24 21:53:24 +0300

    Gauge reacts to firerate

--- scripts/SpawnBullet.gd
+++ scripts/SpawnBullet.gd
@@ -1,13 +1,18 @@
 extends Position2D
 
 onready var firerate_timer = get_parent().get_node("WeaponFirerate")
+onready var firerate_gauge = get_parent().get_parent().get_node("HUD/FirerateGauge")
 
 const bullet_scene = preload("res://scenes/PlayerBullet.tscn")
 
 var firerate = 0.3
 
 func _process(delta):
+	firerate_gauge.max_value = firerate
+	firerate_gauge.value = firerate_timer.time_left
+	
 	if Input.is_action_pressed("shoot"):
+		print(firerate_timer.time_left)
 		if firerate_timer.time_left == 0:
 			var bullet = bullet_scene.instance()
 			bullet.position = global_position

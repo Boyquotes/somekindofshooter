4e5b7360ec6605be12ab02f3a7a9c82afcea206d
ady <ady84.ro@gmail.com>
2023-05-24 16:33:01 +0300

    Delete off scren bullets

--- scripts/PlayerBulletMovement.gd
+++ scripts/PlayerBulletMovement.gd
@@ -5,3 +5,7 @@ const SPEED = 10
 func _physics_process(delta):
 	var velocity = Vector2.RIGHT * SPEED
 	position += velocity.rotated(rotation)
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
